01d5b467e69b3f23252617b57f6377d20d72a52d
WilliamAE <erazowilliam253@gmail.com>
2025-05-05 16:44:03 -0500

    creacion de modelos

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Producto, Categoria, Valor, Cantidad
 
 # Register your models here.
+admin.site.register(Producto)
+admin.site.register(Categoria)
+admin.site.register(Valor)
+admin.site.register(Cantidad)
--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    cantidad = models.IntegerField()
 
+    def __str__(self):
+        return self.nombre
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Valor(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.producto.nombre} - {self.valor}"
+
+class Cantidad(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.producto.nombre} - {self.cantidad}"
